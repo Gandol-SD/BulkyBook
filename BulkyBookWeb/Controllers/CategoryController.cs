41d49100750b03afea42ba7d4a44a59ed57f6ba0
Awab M.Najim <44520076+Gandol-SD@users.noreply.github.com>
2022-10-13 13:58:03 +0200

    Initial commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BulkyBookWeb.Models;
+using BulkyBookWeb.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext dbx;
+
+        public CategoryController(AppDbContext db)
+        {
+            dbx = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> catList = dbx.Categories.ToList();
+
+            return View(catList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+            // Custom Validation Message
+            if(cat.Name == cat.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display order cannot match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                dbx.Categories.Add(cat);
+                dbx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cat);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BulkyBook
+ MVC Course - Learning
