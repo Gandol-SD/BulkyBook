9b2682e46f2f5da40f90ffacdcf84bbcf5d115e6
Awab M.Najim <44520076+Gandol-SD@users.noreply.github.com>
2022-10-16 09:10:40 +0200

    Add Edit/Delete to Category Controller

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -38,12 +38,90 @@ namespace BulkyBookWeb.Controllers
 
             if (ModelState.IsValid)
             {
+                cat.CreatedDateTime = DateTime.Now;
                 dbx.Categories.Add(cat);
                 dbx.SaveChanges();
+                TempData["successmsg"] = cat.Name + " Category Created Successfully!";
                 return RedirectToAction("Index");
             }
 
             return View(cat);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var catFromDb = dbx.Categories.Find(id);
+            if(catFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category cat)
+        {
+            // Custom Validation Message
+            if (cat.Name == cat.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display order cannot match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                dbx.Categories.Update(cat);
+                dbx.SaveChanges();
+                TempData["successmsg"] = cat.Name + " Category Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(cat);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var catFromDb = dbx.Categories.Find(id);
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var catFromDb = dbx.Categories.Find(id);
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+
+            dbx.Categories.Remove(catFromDb);
+            dbx.SaveChanges();
+            TempData["successmsg"] = catFromDb.Name + " Category Removed Successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
